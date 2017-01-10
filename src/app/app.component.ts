50cf8cffda2378ef1115cc5fb458bf011eac7de4
diegofsza <diegofsza@gmail.com>
2017-01-10 18:28:58 -0300

    Updated demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from './data.service';
+import {Observable} from 'rxjs/Observable';
+import {FormGroup, FormBuilder, FormControl} from '@angular/forms';
+import {IqSelect2Item} from 'ng2-iq-select2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    public form: FormGroup;
+    public listItems: (term: string) => Observable<IqSelect2Item[]>;
+    public getItems: (ids: string[]) => Observable<IqSelect2Item[]>;
+
+    constructor(private dataService: DataService,
+                private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            countrySingle: [{
+                value: {
+                    'id': '8',
+                    'text': 'Argentina',
+                    'entity': {
+                        'id': '8',
+                        'name': 'Argentina',
+                        'money': 'ARS'
+                    }
+                },
+                disabled: true
+            }],
+            countryMultiple: null,
+            countryMultipleDisabled: new FormControl({
+                value: [{
+                    'id': '8', 'text': 'Argentina', 'entity': {
+                        'id': '8',
+                        'name': 'Argentina',
+                        'money': 'ARS'
+                    }
+                }],
+                disabled: true
+            }),
+            countrySingleMin0: null,
+            countryMultipleMin0: null
+        });
+
+        this.listItems = this.listData().bind(this.dataService);
+        this.getItems = this.getCurrentItems().bind(this.dataService);
+    }
+
+    listData(): (term: string) => Observable < IqSelect2Item[] > {
+        return this.dataService.listData;
+    }
+
+    getCurrentItems(): (ids: string[]) => Observable < IqSelect2Item[] > {
+        return this.dataService.getItems;
+    }
+
+    send(formJson: string) {
+        console.log(formJson);
+    }
+
+    onSelect(item: IqSelect2Item) {
+        console.log('Item selected: ' + item.text);
+    }
+
+    onRemove(item: IqSelect2Item) {
+        console.log('Item removed: ' + item.text);
+    }
+
+    reset() {
+        console.log('Resetting form');
+        this.form.reset();
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {DataService} from './data.service';
+import {IqSelect2Module} from 'ng2-iq-select2';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpModule,
+        IqSelect2Module
+    ],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
